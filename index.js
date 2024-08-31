0ec63b085b58238b678b7735ac3cf6825242f832
mmedoo <mmedoo.active@gmail.com>
2024-08-31 15:34:06 +0300

    chore: Update npm dependencies and add concurrent script for development

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ const importRouter = require('./routes/import');
 const exportRouter = require('./routes/export');
 const path = require('path');
 const app = express();
+const cors = require('cors');
 
 app.use(express.static(path.join(__dirname, 'public/build')));
 
 app.use(express.json());
+ 
+// app.use(cors());
 
 app.use('/', importRouter);
 app.use('/', exportRouter);
 
-app.listen(8080);
+app.listen(8080, () => {
+	console.log('Server is running on port 8080');
+});
 
 module.exports = app;
